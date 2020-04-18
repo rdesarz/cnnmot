1c976f2fc116b7b73ad73f54c292320aa0854973
rdesarz <hib.pub@protonmail.com>
2020-04-18 17:11:52 +0200

    Add helper function in yolov3 model

--- modules/cnn_models/yolov3.py
+++ modules/cnn_models/yolov3.py
@@ -6,6 +6,10 @@ from keras.layers.merge import add, concatenate
 from keras.models import Model
 import struct
 import cv2
+from keras.preprocessing.image import load_img, img_to_array
+from numpy import expand_dims
+from matplotlib import pyplot
+from matplotlib.patches import Rectangle
 
 os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
 os.environ["CUDA_VISIBLE_DEVICES"] = "0"
@@ -405,3 +409,62 @@ def draw_boxes(image, boxes, labels, obj_thresh):
                         (0, 255, 0), 2)
 
     return image
+
+
+# load and prepare an image
+def load_image_pixels(filename, shape):
+    # load the image to get its shape
+    image = load_img(filename)
+    width, height = image.size
+    # load the image with the required size
+    image = load_img(filename, target_size=shape)
+    # convert to numpy array
+    image = img_to_array(image)
+    # scale pixel values to [0, 1]
+    image = image.astype('float32')
+    image /= 255.0
+    # add a dimension so that we have one sample
+    image = expand_dims(image, 0)
+    return image, width, height
+
+
+# get all of the results above a threshold
+def get_boxes(boxes, labels, thresh):
+    v_boxes, v_labels, v_scores = list(), list(), list()
+    # enumerate all boxes
+    for box in boxes:
+        # enumerate all possible labels
+        for i in range(len(labels)):
+            # check if the threshold for this label is high enough
+            if box.classes[i] > thresh:
+                v_boxes.append(box)
+                v_labels.append(labels[i])
+                v_scores.append(box.classes[i] * 100)
+                # don't break, many labels may trigger for one box
+    return v_boxes, v_labels, v_scores
+
+
+# draw all results
+def draw_boxes(filename, v_boxes, v_labels, v_scores):
+    # load the image
+    data = pyplot.imread(filename)
+    # plot the image
+    pyplot.imshow(data)
+    # get the context for drawing boxes
+    ax = pyplot.gca()
+    # plot each box
+    for i in range(len(v_boxes)):
+        box = v_boxes[i]
+        # get coordinates
+        y1, x1, y2, x2 = box.ymin, box.xmin, box.ymax, box.xmax
+        # calculate width and height of the box
+        width, height = x2 - x1, y2 - y1
+        # create the shape
+        rect = Rectangle((x1, y1), width, height, fill=False, color='white')
+        # draw the box
+        ax.add_patch(rect)
+        # draw text and score in top left corner
+        label = "%s (%.3f)" % (v_labels[i], v_scores[i])
+        pyplot.text(x1, y1, label, color='white')
+    # show the plot
+    pyplot.show()
